6bce698f005e35267de74e73c1836d779dd073c4
morel-porel <paciomuriel@gmail.com>
2025-10-13 14:03:03 +0800

    refactor: Correct monorepo structure and fix backend nesting

---
+++ recipecart-backend/src/main/java/com/palepo/recipecart/RecipecartApplication.java
@@ -0,0 +1,13 @@
+package com.palepo.recipecart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipecartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipecartApplication.class, args);
+	}
+
+}
---
+++ recipecart-backend/src/test/java/com/palepo/recipecart/RecipecartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.palepo.recipecart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipecartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
