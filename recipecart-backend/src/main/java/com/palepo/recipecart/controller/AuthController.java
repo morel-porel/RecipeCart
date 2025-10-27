004e620ecfffea0acdc67210fb3391684c53ca8c
John Luis <L24X10W02@LabHCI.net>
2025-10-27 12:19:50 +0800

    added CrossOrigin to controllers

--- recipecart-backend/src/main/java/com/palepo/recipecart/controller/AuthController.java
+++ recipecart-backend/src/main/java/com/palepo/recipecart/controller/AuthController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins="http://localhost:5173")
 public class AuthController {
 
     private final UserService userService;
--- recipecart-backend/src/main/java/com/palepo/recipecart/controller/OrderController.java
+++ recipecart-backend/src/main/java/com/palepo/recipecart/controller/OrderController.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins="http://localhost:5173")
 public class OrderController {
 
     private final OrderService orderService;
