2bed293acb0163943ea69a0f59b4f73bf63efafc
morel-porel <paciomuriel@gmail.com>
2025-11-29 23:29:36 +0800

    fix: add annotations to prevent circular reference in User entities

--- recipecart-backend/src/main/java/com/palepo/recipecart/entity/UserProfile.java
+++ recipecart-backend/src/main/java/com/palepo/recipecart/entity/UserProfile.java
@@ -1,5 +1,6 @@
 package com.palepo.recipecart.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import java.util.Set;
 
@@ -13,6 +14,7 @@ public class UserProfile {
     @OneToOne
     @MapsId // This links this entity's ID to the User's ID
     @JoinColumn(name = "user_id")
+    @JsonBackReference
     private User user;
 
     @ElementCollection
