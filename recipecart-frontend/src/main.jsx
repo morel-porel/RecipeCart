0f3c1720b5b112e39e584e1e2b75b2df1f0825c7
Andre-12-maker <andre.policios@cit.edu>
2025-12-05 17:40:03 +0800

    fix: Order Summary path

--- recipecart-frontend/src/main.jsx
+++ recipecart-frontend/src/main.jsx
@@ -70,9 +70,6 @@ const router = createBrowserRouter([
       {
         path: 'user-orders',   // URL: /user-orders
         element: <UserOrders />,
-      }
-        path: 'order-summary',   // URL: /user-orders
-        element: <OrderPage />,
       },
       {
         path: 'add-recipe',
